008ae397bc94defdb16a35d41b22933228e2510a
James 3 <james+someoneelse3@nrwl.io>
2024-11-11 22:31:25 +0400

    chore: wip

--- usage.js
+++ usage.js
@@ -1,6 +1,7 @@
 const cp = require("child_process");
 
-for (let i = 100; i < 101; i++) {
+for (let i = 0; i < 101; i++) {
+  console.log(`Running test${i}`);
   cp.execSync(
     `NX_BRANCH=test${i} NX_CI_EXECUTION_ID=${i} npx nx run-many -t build`,
     { stdio: "inherit" }
